5cf6d0a35aff6668d6fee6359d8b8f2f5c7d81f7
Angeline <angelineinfante892@gmail.com>
2020-09-24 21:40:24 -0400

    Final commit, I will become a react progit status

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="personal picture" />
         <p>
           My name is Angeline Infante, and I am going to master React!
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="Github-link"
+          href="https://git.io/JeETH "
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check me out!
         </a>
       </header>
     </div>
